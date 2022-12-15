2dbe96741552b897234e01da13f3ee206a151feb
Si Hyeong Lee <super2451894@gmail.com>
2022-12-15 22:52:54 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
   win.loadFile("index.html");
